6169bf417c1c6da50712498d0cef10d2295ac11a
Brutosippon <joaoccfidalgo@gmail.com>
2022-07-27 12:23:09 -0100

    heroku

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 
 /* Configurar portas para heroku
 const express = require('express');
-const apps = express();
+const app = express();
 const path = require('path');
-const port = process.env.PORT || 5000 || 3000;
-apps.listen(port, (err) =>{
+const port = process.env.PORT || 5000;
+app.listen(port, (err) =>{
   if (err) return console.log(err);
   console.log('Server running on port: ', port)
 }) 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static('build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  });
+ }
 */
 
 const express = require("express");
@@ -25,7 +31,7 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
   });
  }// end get
- 
+
 app.listen(port, (err) =>{
   if (err) return console.log(err);
   console.log('Server running on port: ', port)
