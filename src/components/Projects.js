ac7f1308dfce6cfd5b5e01cd2d112fe9746707da
Brutosippon <joaoccfidalgo@gmail.com>
2022-08-02 11:14:57 -0100

    tentativa de corrigir erro deploy heroku

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -78,6 +78,7 @@ export const Projects = () => {
                                 key={index}
                                 {...project}
                                 />
+                                
                             )
                           })
                         }
